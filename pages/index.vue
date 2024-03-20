4eea2fa404b7153365dc8c973e8e7ca5b860639e
Nicolas Giard <github@ngpixel.com>
2024-03-20 19:29:57 -0400

    chore: Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-3xl text-center">
             <div class="mt-5"><img src="~/assets/logo-text-dark.png" alt="DraftForge" /></div>
-            <h1 class="text-4xl font-bold tracking-tight text-white mt-5 sm:text-3xl">Write, review and publish Internet Drafts / RFCs</h1>
+            <h1 class="text-4xl font-bold tracking-tight text-white mt-5 sm:text-3xl">Write, Review, Refine and Submit Internet-Drafts</h1>
             <p class="mt-6 text-md leading-8 text-gray-300">DraftForge is under active development. A beta release will be available in the coming months.</p>
             <div class="mt-10 flex flex-col justify-center items-center gap-6">
               <a href="https://github.com/ietf-tools/editor/releases/latest" class="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700">Download Alpha Release</a>
