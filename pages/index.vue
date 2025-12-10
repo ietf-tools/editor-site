70d8e047d1ee100e98b17f5e100a7c1654b65133
Nicolas Giard <github@ngpixel.com>
2025-12-10 02:05:03 -0500

    chore: Update GitHub links in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@
             <h1 class="text-4xl font-bold tracking-tight text-white mt-5 sm:text-3xl">Write, Review, Refine and Submit Internet-Drafts</h1>
             <p class="mt-6 text-md leading-8 text-gray-300">DraftForge is under active development. A beta release will be available in the coming months.</p>
             <div class="mt-10 flex flex-col justify-center items-center gap-6">
-              <a href="https://github.com/ietf-tools/editor/releases/latest" class="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700">Download Alpha Release</a>
+              <a href="https://marketplace.visualstudio.com/items?itemName=ietf.draftforge" class="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700">Download Alpha Release</a>
               <div class="flex justify-center items-center gap-x-6">
-                <a href="https://github.com/ietf-tools/editor" class="text-sm font-semibold leading-6 text-blue-200">GitHub</a>
-                <a href="https://github.com/ietf-tools/editor/issues" class="text-sm font-semibold leading-6 text-blue-200">Report Bug</a>
-                <a href="https://github.com/ietf-tools/editor/blob/main/LICENSE" class="text-sm font-semibold leading-6 text-blue-200">License</a>
+                <a href="https://github.com/ietf-tools/draftforge" class="text-sm font-semibold leading-6 text-blue-200">GitHub</a>
+                <a href="https://github.com/ietf-tools/draftforge/issues" class="text-sm font-semibold leading-6 text-blue-200">Report Bug</a>
+                <a href="https://github.com/ietf-tools/draftforge/blob/main/LICENSE" class="text-sm font-semibold leading-6 text-blue-200">License</a>
               </div>
             </div>
             <div class="flex justify-center mt-12">
